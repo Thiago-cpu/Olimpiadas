efe7e1bedcd86a2b70e91f2396a242ea320d3cd8
Thiago-cpu <thifran789456323@gmail.com>
2021-10-01 14:24:54 -0300

    Borrando-ServerCopia

--- Server - copia/src/utils/extractNullProps.ts
+++
@@ -1,7 +0,0 @@
-export function extractNullProps(obj: any): any{
-    return Object.fromEntries(
-        Object.entries(obj)
-          .filter(([_, v]) => v != null)
-          .map(([k, v]) => [k, v === Object(v) ? extractNullProps(v) : v])
-      );
-}
